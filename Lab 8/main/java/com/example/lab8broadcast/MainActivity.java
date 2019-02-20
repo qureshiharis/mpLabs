d24b0585eec6dcc234b2582686af41fd7915cfdf
qureshiharis <38608902+qureshiharis@users.noreply.github.com>
2019-02-20 19:18:13 +0500

    Add files via upload

---
+++ Lab 8/main/java/com/example/lab8broadcast/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.lab8broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public class SMSBroadcastReceiver extends BroadcastReceiver {
+
+        private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+        private static final String TAG = "SMSBroadcastReceiver";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "Intent received: " + intent.getAction());
+
+            if (intent.getAction().equals(SMS_RECEIVED)) {
+                Bundle bundle = intent.getExtras();
+                if (bundle != null) {
+                    Object[] pdus = (Object[])bundle.get("pdus");
+                    final SmsMessage[] messages = new SmsMessage[pdus.length];
+                    for (int i = 0; i < pdus.length; i++) {
+                        messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                    }
+                    if (messages.length > -1) {
+                        Log.i(TAG, "Message received: " + messages[0].getMessageBody());
+                    }
+                }
+            }
+        }
+    }
+}
